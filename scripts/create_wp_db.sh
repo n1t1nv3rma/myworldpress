a3ecea3c963cba6390678c9203d4c5f1804d9fe3
n1t1nv3rma <nitinsheenu@yahoo.com>
2015-07-10 14:50:53 +1000

    Update create_wp_db.sh

--- scripts/create_wp_db.sh
+++ scripts/create_wp_db.sh
@@ -3,9 +3,19 @@ MYSQL_ROOT_PASSWD="P@ssw0rd"
 WP_DATABASE="worldpress"
 WP_DB_USER="wpadmin"
 WP_DB_PASSWD="P@ssw0rd"
+
 mysql --host=localhost --user=root --password=$MYSQL_ROOT_PASSWD << EOF
+ show profiles;
+EOF
+
+if [ $? -ne 0 ] 
+then
+ mysql --host=localhost --user=root --password=$MYSQL_ROOT_PASSWD << EOF
  CREATE DATABASE $WP_DATABASE;
  GRANT ALL PRIVILEGES ON $WP_DATABASE.* TO "$WP_DB_USER"@"localhost" IDENTIFIED BY "$WP_DB_PASSWD";
  FLUSH PRIVILEGES;
  EXIT
 EOF
+else
+ echo "skipping DB creation... DB exists."
+fi
